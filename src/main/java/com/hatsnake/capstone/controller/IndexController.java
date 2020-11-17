678adec40b6532fe9da38aae6d532a4461ca04bb
hatsnake <hatsnake@naver.com>
2020-11-17 18:12:50 +0900

    naver Login
    naver Login

--- src/main/java/com/hatsnake/capstone/controller/IndexController.java
+++ src/main/java/com/hatsnake/capstone/controller/IndexController.java
@@ -1,5 +1,7 @@
 package com.hatsnake.capstone.controller;
 
+import com.hatsnake.capstone.config.auth.LoginUser;
+import com.hatsnake.capstone.config.dto.SessionUser;
 import com.hatsnake.capstone.dto.TourListResponseDto;
 import com.hatsnake.capstone.service.tourList.TourListService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,6 +11,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpSession;
 import java.util.List;
 
 @Controller
@@ -17,13 +20,20 @@ public class IndexController {
     @Autowired
     private TourListService tourListService;
 
+    @Autowired
+    private HttpSession httpSession;
+
     @GetMapping("")
-    public String index(Model model, @RequestParam(value = "page", defaultValue = "1") Integer pageNum) {
+    public String index(Model model, @RequestParam(value = "page", defaultValue = "1") Integer pageNum, @LoginUser SessionUser user) {
 
         List<TourListResponseDto> tourListPagination = tourListService.findAllPagination(pageNum);
         Integer[] pageList = tourListService.getPageList(pageNum);
         List<TourListResponseDto> tourListAll = tourListService.findAll();
 
+        if(user != null) {
+            model.addAttribute("userPicture", user.getPicture());
+            model.addAttribute("userName", user.getName());
+        }
 
         model.addAttribute("tourListPagination", tourListPagination);
         model.addAttribute("pageList", pageList);
