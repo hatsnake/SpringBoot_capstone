edd0f096a87321be12221a1ab57541e60453fa32
hatsnake <hatsnake@naver.com>
2020-11-17 19:08:51 +0900

    remove OAuth Login
    remove OAuth Login

--- src/main/java/com/hatsnake/capstone/controller/IndexController.java
+++ src/main/java/com/hatsnake/capstone/controller/IndexController.java
@@ -1,7 +1,5 @@
 package com.hatsnake.capstone.controller;
 
-import com.hatsnake.capstone.config.auth.LoginUser;
-import com.hatsnake.capstone.config.dto.SessionUser;
 import com.hatsnake.capstone.dto.TourListResponseDto;
 import com.hatsnake.capstone.service.tourList.TourListService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -24,16 +22,11 @@ public class IndexController {
     private HttpSession httpSession;
 
     @GetMapping("")
-    public String index(Model model, @RequestParam(value = "page", defaultValue = "1") Integer pageNum, @LoginUser SessionUser user) {
+    public String index(Model model, @RequestParam(value = "page", defaultValue = "1") Integer pageNum) {
 
         List<TourListResponseDto> tourListPagination = tourListService.findAllPagination(pageNum);
         Integer[] pageList = tourListService.getPageList(pageNum);
         List<TourListResponseDto> tourListAll = tourListService.findAll();
-
-        if(user != null) {
-            model.addAttribute("userPicture", user.getPicture());
-            model.addAttribute("userName", user.getName());
-        }
 
         model.addAttribute("tourListPagination", tourListPagination);
         model.addAttribute("pageList", pageList);
