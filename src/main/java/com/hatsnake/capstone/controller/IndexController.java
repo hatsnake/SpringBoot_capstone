335608ed22e3be41c305641f78346f9e4c82c424
hatsnake <hatsnake@naver.com>
2020-11-19 01:07:07 +0900

    retry Naver Login
    retry Naver Login

--- src/main/java/com/hatsnake/capstone/controller/IndexController.java
+++ src/main/java/com/hatsnake/capstone/controller/IndexController.java
@@ -1,5 +1,7 @@
 package com.hatsnake.capstone.controller;
 
+import com.hatsnake.capstone.config.auth.LoginUser;
+import com.hatsnake.capstone.config.dto.SessionUser;
 import com.hatsnake.capstone.dto.TourListResponseDto;
 import com.hatsnake.capstone.service.tourList.TourListService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -24,11 +26,16 @@ public class IndexController {
     private HttpSession httpSession;
 
     @GetMapping("")
-    public String index(Model model, @RequestParam(value = "page", defaultValue = "1") Integer pageNum) {
+    public String index(Model model, @RequestParam(value = "page", defaultValue = "1") Integer pageNum, @LoginUser SessionUser user) {
 
         List<TourListResponseDto> tourListPagination = tourListService.findAllPagination(pageNum);
         Integer[] pageList = tourListService.getPageList(pageNum);
         List<TourListResponseDto> tourListAll = tourListService.findAll();
+
+        if(user != null) {
+            model.addAttribute("userPicture", user.getPicture());
+            model.addAttribute("userName", user.getName());
+        }
 
         model.addAttribute("tourListPagination", tourListPagination);
         model.addAttribute("pageList", pageList);
@@ -42,16 +49,32 @@ public class IndexController {
     @ResponseBody
     public Object getTourList(@RequestParam("id") Long id) {
         TourListResponseDto tourList = tourListService.getTourList(id);
+
         HashMap<String, Object> map = new HashMap<String, Object>();
         map.put("id", tourList.getId());
-        map.put("title", tourList.getTitle());
-        map.put("cotAddrNew", tourList.getCotAddrNew());
-        map.put("cotTel", tourList.getCotTel());
-        map.put("cotHomepage", tourList.getCotHomepage());
-        map.put("wifiUse", tourList.getWifiUse());
-        map.put("cotCloseDay", tourList.getCotCloseDay());
-        map.put("cotUseTimeDesc", tourList.getCotUseTimeDesc());
-        map.put("cotTroublemanConvenfac", tourList.getCotTroublemanConvenfac());
+        map.put("title","<i class=\"map marker alternate icon\"></i> " +tourList.getTitle());
+        map.put("cotAddrNew", "<i class=\"map icon\"></i> " +tourList.getCotAddrNew());
+        if(tourList.getWifiUse().equals("Y")) {
+            map.put("wifiUse", "<i class=\"wifi icon\"></i> 와이파이");
+        } else {
+            map.put("wifiUse", "<i class=\"ban icon\"></i> 와이파이");
+        }
+        if(tourList.getCotTel() != null) {
+            map.put("cotTel", "<i class=\"phone icon\"></i> " + tourList.getCotTel());
+        }
+        //홈페이지 (http://)추가
+        if(tourList.getCotHomepage() != null) {
+            map.put("cotHomepage", "<i class=\"laptop house icon\"></i> <a href=\"" + tourList.getCotHomepage() + "\">링크</a>");
+        }
+        if(tourList.getCotCloseDay() != null) {
+            map.put("cotCloseDay", "<i class=\"door closed icon\"></i> " + tourList.getCotCloseDay());
+        }
+        if(tourList.getCotUseTimeDesc() != null) {
+            map.put("cotUseTimeDesc", "<i class=\"door open icon\"></i><br/> " + tourList.getCotUseTimeDesc());
+        }
+        if(tourList.getCotTroublemanConvenfac() != null) {
+            map.put("cotTroublemanConvenfac", "<i class=\"wheelchair icon\"></i><br/> " + tourList.getCotTroublemanConvenfac());
+        }
         return map;
     }
 
